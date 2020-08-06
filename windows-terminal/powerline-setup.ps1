430e42d581f22ce9d314bf891945a80ed93c14d0
Martin Deiss <martindeiss@gmail.com>
2020-08-06 23:24:33 +0200

    added windows-terminal config scripts

---
+++ windows-terminal/powerline-setup.ps1
@@ -0,0 +1,24 @@
+﻿Clear-Host
+
+Write-Host "This will only work with PowerShell 7"
+
+$profilePath = "$Home/Documents/Powershell/Microsoft.PowerShell_profile.ps1"
+$profilePathTest = test-path $profilePath
+
+Write-Host "Installing posh-git"
+
+Install-Module posh-git -Scope CurrentUser
+Install-Module oh-my-posh -Scope CurrentUser
+Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
+
+If ($profilePathTest -eq $True) {
+    Write-Host "Profile is already created!"
+    exit 0
+}
+
+Write-Host "Create Profile"
+New-Item $profilePath -ItemType File -Force
+
+Add-Content $profilePath "Import-Module posh-git"
+Add-Content $profilePath "Import-Module oh-my-posh"
+Add-Content $profilePath "Set-Theme Paradox"
---
+++ windows-terminal/settings-setup.ps1
@@ -0,0 +1,3 @@
+﻿Write-Host "Copy Windows-Terminal settings.json"
+
+Copy-Item settings.json (Join-Path (Get-ChildItem -Path $env:userprofile\AppData\Local\Packages -Filter "Microsoft.WindowsTerminal_*" -Directory).Fullname "LocalState") -Force
