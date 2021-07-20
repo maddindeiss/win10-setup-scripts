d3e93e697146e84baa47499e4212894e7a0bff1a
Martin Deiss <deiss@gardenofconcepts.com>
2021-07-20 15:21:30 +0200

    added PSReadLine and Terminal-Icons, added options for PSReadLine

--- windows-terminal/powerline-setup.ps1
+++ windows-terminal/powerline-setup.ps1
@@ -9,7 +9,8 @@ Write-Host "Installing posh-git"
 
 Install-Module posh-git -Scope CurrentUser
 Install-Module oh-my-posh -Scope CurrentUser
-Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
+Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck -AllowPrerelease
+Install-Module -Name Terminal-Icons -Repository PSGallery
 
 If ($profilePathTest -eq $True) {
     Write-Host "Profile is already created!"
@@ -21,4 +22,14 @@ New-Item $profilePath -ItemType File -Force
 
 Add-Content $profilePath "Import-Module posh-git"
 Add-Content $profilePath "Import-Module oh-my-posh"
-Add-Content $profilePath "Set-Theme Paradox"
+Add-Content $profilePath "Import-Module PSReadLine"
+Add-Content $profilePath "Import-Module -Name Terminal-Icons"
+Add-Content $profilePath "Set-PoshPrompt -Theme Paradox"
+
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
