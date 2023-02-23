36a98762eed4d29889d77366535f1f1f02495b33
Martin Deiss <martindeiss@gmail.com>
2023-02-23 22:02:17 +0100

    Added ssh.exe path to the git config

--- setup-git.ps1
+++ setup-git.ps1
@@ -1,3 +1,4 @@
 git config --global core.eol lf
 git config --global core.autocrlf input
 git config --global core.symlinks true
+git config --global core.sshCommand "C:/Windows/System32/OpenSSH/ssh.exe"
